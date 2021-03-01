28d85d095651272bc669d164603675225eba51a6
valeroMC <valerocomingsoon@gmail.com>
2021-03-01 15:08:18 -0500

    cambios en el template

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -2,16 +2,40 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
-  //templateUrl: './empleado.component.html',
-  template:"<p>Aqui iría un empleado</p>",
+  templateUrl: './empleado.component.html',
+  //template:"<p>Aqui iría un empleado</p>",
   //styleUrls: ['./empleado.component.css']
   styles:["p{background-color:#0858eb11;}"]
 })
 export class EmpleadoComponent implements OnInit {
 
+  nombre="juan"
+  apellido="Díaz";
+  //private
+   edad=28;
+  //empresa="Microsoft";
   constructor() { }
+ /*getEdad(){
+      return this.edad;
+ }*/
 
-  ngOnInit(): void {
-  }
+  habilitacionCuadro=false;
 
-}
+  URegistrado=false;
+
+    llamaEmpresa(value:String){
+      
+    }
+
+    getRegistroUsuario(){
+      this.URegistrado=false;
+
+
+
+
+    }
+    
+      ngOnInit(): void {
+      }
+
+    }
